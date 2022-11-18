a9588b7a6e1507c8366c99cf92c2464a47b8b874
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-18 23:36:49 +0530

    Update brothers.py

--- Database/brothers.py
+++ Database/brothers.py
@@ -1,25 +1,25 @@
 from . import db
 
-sistersdb = db.sisters
+brothersdb = db.brothers
 
-async def add_sister(s1_id: int, s2_id: int):
-    x = await sistersdb.find_one({"s1_id": s1_id})
+async def add_brother(b1_id: int, b2_id: int):
+    x = await brothersdb.find_one({"b1_id": b1_id})
     if x:
-        list = x["sisters"]
-        if not s2_id in list:
-            list.append(s2_id)
-            return await sistersdb.update_one({"s1_id": s1_id}, {"$set": {"sisters": list}}, upsert=True)
+        list = x["brothers"]
+        if not b2_id in list:
+            list.append(b2_id)
+            return await brothersdb.update_one({"b1_id": b1_id}, {"$set": {"brothers": list}}, upsert=True)
     else:
-        await sistersdb.insert_one({"s1_id": s1_id}, {"$set": {"sisters": [s2_id]}})
+        await brothersdb.insert_one({"b1_id": b1_id}, {"$set": {"brothers": [b2_id]}})
 
 
-async def del_sister(s1_id: int, s2_id: int):
-    x = await sistersdb.find_one({"s1_id": s1_id})
+async def del_sister(b1_id: int, b2_id: int):
+    x = await brothersdb.find_one({"b1_id": b1_id})
     if x:
-        list = x["sisters"]
-        if s2_id in list:
-            list.remove(s2_id)
-            await sistersdb.delete_one({"s1_id": s1_id}, {"$set": {"sisters": list}}, upsert=True)
+        list = x["brothers"]
+        if b2_id in list:
+            list.remove(b2_id)
+            await brothersdb.delete_one({"b1_id": b1_id}, {"$set": {"brothers": list}}, upsert=True)
 
 async def are_sisters(s1_id: int, s2_id: int):
     x = await sistersdb.find_one({"s1_id": s1_id})

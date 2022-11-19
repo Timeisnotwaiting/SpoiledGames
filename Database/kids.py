4bf4e0019459cbd2a419bdc7798b938ac1fb13a8
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 09:56:33 +0530

    Create kids.py

---
+++ Database/kids.py
@@ -0,0 +1,34 @@
+from . import db
+
+kidsdb = db.kids
+
+async def add_kid(p_id: int, k_id: int):
+    x = await kidsdb.find_one({"p_id": p_id})
+    if x:
+        list = x["kids"]
+        list.append(k_id)
+        return await kidsdb.update_one({"p_id": p_id}, {"$set": {"kids": list}}, upsert=True)
+    else:
+        return await kidsdb.insert_one({"p_id": p_id}, {"$set": {"kids": list}})
+
+async def del_kid(p_id: int, k_id: int):
+    x = await kidsdb.find_one({"p_id": p_id})
+    if x:
+        list = x["kids"]
+        list.remove(k_id)
+        return await kidsdb.update_one({"p_id": p_id}, {"$set": {"kids": list}}, upsert=True)
+
+async def is_kid(p_id: int, k_id: int):
+    x = await kidsdb.find_one({"p_id": p_id})
+    if x:
+        list = x["kids"]
+        if k_id in list:
+            return True
+    return False
+
+async def get_kids(p_id: int):
+    x = await kidsdb.find_one({"p_id": p_id})
+    if x:
+        list = x["kids"]
+        return list
+    return []

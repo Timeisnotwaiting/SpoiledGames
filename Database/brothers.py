a484ffbcaab9ae20d79bc8aa3b7dd9b52aabe857
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 09:36:47 +0530

    Update brothers.py

--- Database/brothers.py
+++ Database/brothers.py
@@ -13,7 +13,7 @@ async def add_brother(b1_id: int, b2_id: int):
         await brothersdb.insert_one({"b1_id": b1_id}, {"$set": {"brothers": [b2_id]}})
 
 
-async def del_sister(b1_id: int, b2_id: int):
+async def del_brother(b1_id: int, b2_id: int):
     x = await brothersdb.find_one({"b1_id": b1_id})
     if x:
         list = x["brothers"]
@@ -21,11 +21,11 @@ async def del_sister(b1_id: int, b2_id: int):
             list.remove(b2_id)
             await brothersdb.delete_one({"b1_id": b1_id}, {"$set": {"brothers": list}}, upsert=True)
 
-async def are_sisters(s1_id: int, s2_id: int):
-    x = await sistersdb.find_one({"s1_id": s1_id})
+async def are_brothers(b1_id: int, b2_id: int):
+    x = await brothersdb.find_one({"b1_id": b1_id})
     if x:
-        list = x["sisters"]
-        if s2_id in list:
+        list = x["brothers"]
+        if b2_id in list:
             return True
     return False
 

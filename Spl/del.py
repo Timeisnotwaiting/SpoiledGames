8ff83ccd21421c9f6ff876a230f88ad62c5ad79e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 22:35:38 +0530

    Update del.py

--- Spl/del.py
+++ Spl/del.py
@@ -1,11 +1,11 @@
-from Database.parents import parentsdb
+from Database.brothers import brothersdb
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command("del"))
 async def dele(_, m):
-    k_id = int(m.text.split()[1])
+    b1_id = int(m.text.split()[1])
     try:
-        await parentsdb.delete_one({"k_id": k_id})
+        await brothersdb.delete_one({"b1_id": b1_id})
         await m.reply("deleted")
     except Exception as e:
         print(e)

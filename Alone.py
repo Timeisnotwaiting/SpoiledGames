046adebf609455df275ff36dd24b4bdb6adcd9d0
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 17:15:41 +0530

    Create Alone.py

---
+++ Alone.py
@@ -0,0 +1,9 @@
+from pyrogram import Client, idle
+from config import *
+
+AlphaAlone = Client(":alone:", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root("Spl")))
+
+AlphaAlone.start()
+print("None")
+idle()
+

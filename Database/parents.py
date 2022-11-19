136005c17d975308403f37e6249bbc965c11c566
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 09:47:47 +0530

    Create parents.py

---
+++ Database/parents.py
@@ -0,0 +1,34 @@
+from . import db
+
+parentsdb = db.parents
+
+async def add_parent(k_id: int, p_id: int):
+    x = await parentsdb.find_one({"k_id": k_id})
+    if x:
+        list = x["parents"]
+        list.append(p_id)
+        return await parentsdb.update_one({"k_id": k_id}, {"$set": {"parents": list}}, upsert=True)
+    else:
+        return await parentsdb.insert_one({"k_id": k_id}, {"$set": {"parents": list}})
+
+async def del_parent(k_id: int, p_id: int):
+    x = await parentsdb.find_one({"k_id": k_id})
+    if x:
+        list = x["parents"]
+        list.remove(p_id)
+        return await parentsdb.update_one({"k_id": k_id}, {"$set": {"parents": list}}, upsert=True)
+
+async def is_parent(k_id: int, p_id: int):
+    x = await parentsdb.find_one({"k_id": k_id})
+    if x:
+        list = x["parents"]
+        if p_id in list:
+            return True
+    return False
+
+async def get_parents(k_id: int):
+    x = await parentsdb.find_one({"k_id": k_id})
+    if x:
+        list = x["parents"]
+        return list
+    return []

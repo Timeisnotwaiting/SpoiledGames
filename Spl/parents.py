1ea1219163e028f9cfba4b1d5f3b38aee4b8a548
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 16:15:30 +0530

    Update parents.py

--- Spl/parents.py
+++ Spl/parents.py
@@ -3,8 +3,21 @@ from Database.kids import *
 from Database.genders import *
 from pyrogram import Client, filters
 
+k_id = None
+p_id = None
+kid_g = None
+parent_g = None
+k_fn = None
+p_fn = None
+
 @Client.on_message(filters.command("parent", ["/", "?", "!", "."]))
 async def parents_add(_, m):
+    global k_id
+    global p_id
+    global kid_g
+    global parent_g
+    global k_fn
+    global p_fn
     k_id = m.from_user.id
     if m.reply_to_message:
         p_id = m.reply_to_message.from_user.id
@@ -35,4 +48,35 @@ async def parents_add(_, m):
     p_fn = (await _.get_users(p_id)).first_name
     await _.send_message(m.chat.id, "{} wants {} as {} {}".format(k_fn, p_fn, "his" if kid_g == "male" else "her", "Dad" if parent_g == "male" else "Mom"), reply_markup=markup)
 
-    
+@Client.on_callback_query(filters.regex("parent_accept"))
+async def acc(_, q):
+    global k_id
+    global p_id
+    global kid_g
+    global parent_g
+    global k_fn
+    global p_fn
+    if q.from_user.id != p_id:
+        return await q.answer("This is not for you baka !", show_alert=True)
+    await add_parent(k_id, p_id)
+    await add_kid(p_id, k_id)
+    return await q.edit_message_text("{} accepted {} as {} {}".format(p_fn, k_fn, "his" if parent_g=="male" else "her", "Son" if kid_g=="male" else "Daughter"))
+
+@Client.on_message(filters.command("leaveparent", ["/", ".", "?", "!"]))
+async def leaveparent(_, m):
+    k_id = m.from_user.id
+    if m.reply_to_message:
+        p_id = m.reply_to_message.from_user.id
+    else:
+        txt = m.text.split()
+        if txt[1][0] == "@":
+            p_id = (await _.get_users(txt[1])).id
+        else:
+            p_id = int(txt[1])
+    x = await is_parent(k_id, p_id)
+    parent_g = await check_gender(p_id)
+    if not x:
+        return await m.reply("{} is not your parent baka !".format("He" if parent_g=="male" else "She"))
+    await del_parent(k_id, p_id)
+    await del_kid(p_id, k_id)
+    await m.reply("parent abandoned !")

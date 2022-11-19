9e103e77590fb8b1ac7db0a7657c4022c30a1c8e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 18:39:42 +0530

    Update del.py

--- Spl/del.py
+++ Spl/del.py
@@ -2,7 +2,7 @@ from Database.parents import del_parent
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command("del"))
-async def del(_, m):
+async def dele(_, m):
     query1 = int(m.text.split()[1])
     query2 = int(m.text.split()[2])
     try:

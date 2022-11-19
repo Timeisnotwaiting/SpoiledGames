b947967b093061deca4d44a857405f1b3c7773e8
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-11-19 18:31:23 +0530

    Update parents.py

--- Database/parents.py
+++ Database/parents.py
@@ -10,7 +10,7 @@ async def add_parent(k_id: int, p_id: int):
         return await parentsdb.update_one({"k_id": k_id}, {"$set": {"parents": list}}, upsert=True)
     else:
         list = [p_id]
-        return await parentsdb.insert_one({"k_id": k_id}, {"$set": {"parents": list}})
+        return await parentsdb.update_one({"k_id": k_id}, {"$set": {"parents": list}}, upsert=True)
 
 async def del_parent(k_id: int, p_id: int):
     x = await parentsdb.find_one({"k_id": k_id})
